8569417543c88042aefe97e59d8fd12d562fa005
Boris Kochergin <kocherginba1992@yandex.ru>
2025-10-09 19:26:29 +0300

    added space

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,7 +2,7 @@ package tasks;
 
 import java.util.ArrayList;
 
-public class Epic extends Task{
+public class Epic extends Task {
     public Epic(String name, String description, TaskStatus status) {
         super(name, description, status);
     }
