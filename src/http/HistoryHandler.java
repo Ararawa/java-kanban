e7880b710587b28e33b9ab65dd62ee82357346fd
Boris Kochergin <kocherginba1992@yandex.ru>
2025-11-01 23:58:14 +0300

    added HistoryHandler;

---
+++ src/http/HistoryHandler.java
@@ -0,0 +1,41 @@
+package http;
+
+import com.google.gson.*;
+import com.sun.net.httpserver.HttpExchange;
+import manager.TaskManager;
+import tasks.Task;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class HistoryHandler extends BaseHttpHandler {
+
+    public HistoryHandler(TaskManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        System.out.println("Началась обработка " + httpExchange.getRequestMethod() + " запроса от клиента.");
+
+        String response = "";
+
+        List<Task> result = manager.getHistory();
+        Gson gson1 = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
+        String jsonOut = gson1.toJson(result);
+        response = jsonOut;
+        httpExchange.getResponseHeaders().set("Content-Type", "application/json");
+        httpExchange.sendResponseHeaders(200, 0);
+
+        try (OutputStream os = httpExchange.getResponseBody()) {
+            os.write(response.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
