853818efef336d5b4278cc25d522ade3d267f48e
Boris Kochergin <kocherginba1992@yandex.ru>
2025-11-02 01:01:14 +0300

    changed server manager to file one;

--- src/http/HttpTaskServer.java
+++ src/http/HttpTaskServer.java
@@ -4,16 +4,19 @@ import com.sun.net.httpserver.HttpServer;
 import manager.Managers;
 import manager.TaskManager;
 
+import java.io.File;
 import java.net.InetSocketAddress;
 
 public class HttpTaskServer {
 
     static TaskManager manager;
+    static String filename = "TestFBTM";
+    static String suffix = ".csv";
 
     public static void main(String[] args) throws Exception {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
         httpServer.start();
-        manager = Managers.getDefault();
+        manager = Managers.getFileManipulator(File.createTempFile(filename, suffix));
 
         httpServer.createContext("/tasks", new TaskHandler(manager));
         httpServer.createContext("/subtasks", new SubtaskHandler(manager));
