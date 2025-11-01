6d76d8c7a7a5c916370e1b7ae0b02a3fb67854b1
Boris Kochergin <kocherginba1992@yandex.ru>
2025-11-02 00:49:40 +0300

    added 500 Internal Server Error IOException;

--- src/http/EpicHandler.java
+++ src/http/EpicHandler.java
@@ -21,62 +21,65 @@ public class EpicHandler extends BaseHttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        try {
+            String method = httpExchange.getRequestMethod();
+            System.out.println("Началась обработка " + method + " запроса от клиента.");
 
-        String method = httpExchange.getRequestMethod();
-        System.out.println("Началась обработка " + method + " запроса от клиента.");
+            String path = httpExchange.getRequestURI().getPath();
+            String[] pathArray = path.split("/");
+            Optional<Integer> id;
+            if (pathArray.length > 2) {
+                id = Optional.of(Integer.parseInt(pathArray[2]));
+            } else {
+                id = Optional.empty();
+            }
 
-        String path = httpExchange.getRequestURI().getPath();
-        String[] pathArray = path.split("/");
-        Optional<Integer> id;
-        if (pathArray.length > 2) {
-            id = Optional.of(Integer.parseInt(pathArray[2]));
-        } else {
-            id = Optional.empty();
-        }
-
-        String response = "";
-        switch(method) {
-            case "POST":
-                response = postMethod(httpExchange);
-                break;
-            case "GET":
-                if (pathArray.length == 4) {
-                    System.out.println("getByID");
-                    List<Subtask> epicSubtasks = manager.getSubtasksByEpicID(id.get());
-                    if (epicSubtasks == null) {
-                        response = "No epic with this id = " + id;
-                        System.out.println(response);
-                        httpExchange.sendResponseHeaders(404, 0);
+            String response = "";
+            switch (method) {
+                case "POST":
+                    response = postMethod(httpExchange);
+                    break;
+                case "GET":
+                    if (pathArray.length == 4) {
+                        System.out.println("getByID");
+                        List<Subtask> epicSubtasks = manager.getSubtasksByEpicID(id.get());
+                        if (epicSubtasks == null) {
+                            response = "No epic with this id = " + id;
+                            System.out.println(response);
+                            httpExchange.sendResponseHeaders(404, 0);
+                        } else {
+                            GsonBuilder gb1 = new GsonBuilder();
+                            Gson gson1 = gb1
+                                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                                    .create();
+                            response = gson1.toJson(epicSubtasks);
+                            httpExchange.getResponseHeaders().set("Content-Type", "application/json");
+                            httpExchange.sendResponseHeaders(200, 0);
+                        }
+                    } else if (id.isPresent()) {
+                        response = getByID(id.get(), httpExchange);
                     } else {
-                        GsonBuilder gb1 = new GsonBuilder();
-                        Gson gson1 = gb1
-                                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
-                                .create();
-                        response = gson1.toJson(epicSubtasks);
-                        httpExchange.getResponseHeaders().set("Content-Type", "application/json");
-                        httpExchange.sendResponseHeaders(200, 0);
+                        response = getAll(httpExchange, pathArray[1]);
                     }
-                } else if (id.isPresent()) {
-                    response = getByID(id.get(), httpExchange);
-                } else {
-                    response = getAll(httpExchange, pathArray[1]);
-                }
-                break;
-            case "DELETE":
-                response = "Вы использовали метод DELETE!";
-                if (id.isPresent()) {
-                    System.out.println("deleteByID");
-                    manager.deleteByID(id.get());
-                    httpExchange.sendResponseHeaders(200, 0);
-                } else {
-                    response = "Cannot DELETE without id";
-                    System.out.println(response);
-                }
-                break;
-            default:
-                response = "Вы использовали какой-то другой метод!";
+                    break;
+                case "DELETE":
+                    response = "Вы использовали метод DELETE!";
+                    if (id.isPresent()) {
+                        System.out.println("deleteByID");
+                        manager.deleteByID(id.get());
+                        httpExchange.sendResponseHeaders(200, 0);
+                    } else {
+                        response = "Cannot DELETE without id";
+                        System.out.println(response);
+                    }
+                    break;
+                default:
+                    response = "Вы использовали какой-то другой метод!";
+            }
+        } catch (IOException e) {
+            httpExchange.sendResponseHeaders(500, 0);
+            response = "Internal Server Error IOException";
         }
-
         try (OutputStream os = httpExchange.getResponseBody()) {
             os.write(response.getBytes(StandardCharsets.UTF_8));
         }
--- src/http/TaskHandler.java
+++ src/http/TaskHandler.java
@@ -18,44 +18,48 @@ public class TaskHandler extends BaseHttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        try {
+            String method = httpExchange.getRequestMethod();
+            System.out.println("Началась обработка " + method + " запроса от клиента.");
 
-        String method = httpExchange.getRequestMethod();
-        System.out.println("Началась обработка " + method + " запроса от клиента.");
+            String path = httpExchange.getRequestURI().getPath();
+            String[] pathArray = path.split("/");
+            Optional<Integer> id;
+            if (pathArray.length > 2) {
+                id = Optional.of(Integer.parseInt(pathArray[2]));
+            } else {
+                id = Optional.empty();
+            }
 
-        String path = httpExchange.getRequestURI().getPath();
-        String[] pathArray = path.split("/");
-        Optional<Integer> id;
-        if (pathArray.length > 2) {
-            id = Optional.of(Integer.parseInt(pathArray[2]));
-        } else {
-            id = Optional.empty();
-        }
-
-        String response = "";
-        switch(method) {
-            case "POST":
-                response = postMethod(httpExchange);
-                break;
-            case "GET":
-                if (id.isPresent()) {
-                    response = getByID(id.get(), httpExchange);
-                } else {
-                    response = getAll(httpExchange, pathArray[1]);
-                }
-                break;
-            case "DELETE":
-                response = "Вы использовали метод DELETE!";
-                if (id.isPresent()) {
-                    System.out.println("deleteByID");
-                    manager.deleteByID(id.get());
-                    httpExchange.sendResponseHeaders(200, 0);
-                } else {
-                    response = "Cannot DELETE without id";
-                    System.out.println(response);
-                }
-                break;
-            default:
-                response = "Вы использовали какой-то другой метод!";
+            String response = "";
+            switch (method) {
+                case "POST":
+                    response = postMethod(httpExchange);
+                    break;
+                case "GET":
+                    if (id.isPresent()) {
+                        response = getByID(id.get(), httpExchange);
+                    } else {
+                        response = getAll(httpExchange, pathArray[1]);
+                    }
+                    break;
+                case "DELETE":
+                    response = "Вы использовали метод DELETE!";
+                    if (id.isPresent()) {
+                        System.out.println("deleteByID");
+                        manager.deleteByID(id.get());
+                        httpExchange.sendResponseHeaders(200, 0);
+                    } else {
+                        response = "Cannot DELETE without id";
+                        System.out.println(response);
+                    }
+                    break;
+                default:
+                    response = "Вы использовали какой-то другой метод!";
+            }
+        } catch (IOException e) {
+            httpExchange.sendResponseHeaders(500, 0);
+            response = "Internal Server Error IOException";
         }
 
         try (OutputStream os = httpExchange.getResponseBody()) {
